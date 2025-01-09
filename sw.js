7e887cf522e2a724ee1a9363b2a6603040f3f2c5
jusamoh <152563308+jusamoh@users.noreply.github.com>
2025-01-09 22:02:22 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,14 +6,34 @@ const aFilesToCache = [
   './app.js'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+// 2.서비스워커를 설치하고 캐시를 저장함
+self.addEventListener('install', pEvent => {
+  console.log('서비스워커 설치함!');
+  pEvent.waitUntil(
+    caches.open(sCacheName)
+    .then(pCache => {
+      console.log('파일을 캐시에 저장함!');
+      return pCache.addAll(aFilesToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+// 3. 고유번호 할당받은 서비스 워커 동작 시작
+self.addEventListener('activate', pEvent => {
+  console.log('서비스워커 동작 시작됨!');
+});
+
+// 4.데이터 요청시 네트워크 또는 캐시에서 찾아 반환 
+self.addEventListener('fetch', pEvent => {
+  pEvent.respondWith(
+    caches.match(pEvent.request)
+    .then(response => {
+      if (!response) {
+        console.log("네트워크에서 데이터 요청!", pEvent.request)
+        return fetch(pEvent.request);
+      }
+      console.log("캐시에서 데이터 요청!", pEvent.request)
+      return response;
+    }).catch(err => console.log(err))
   );
 });
